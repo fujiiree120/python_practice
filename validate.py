899b00288d809b3c4c04f0565156684e754ee2e8
fujiiree120 <fujiiree120@gmail.com>
2021-03-15 22:04:20 +0900

    tuika

--- validate.py
+++ validate.py
@@ -4,10 +4,16 @@ m = re.match('a.c', 'abc')
 print(m.group())
 
 m = re.search('a.c', 'ddddabcdddd')
-print(m.span())
+
 
 m = re.findall('a.c', 'test abc test abd')
-print(m)
+
 
 m = re.match('ab?', 'abn')
+
+
+m = re.match('[a-zA-z]', 'aaaaaa')
+
+
+m = re.match('\s', ' ')
 print(m)
